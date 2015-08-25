17817dab9f59aaa6fadca7c46a50eca24be2f258
Your Name <you@example.com>
2015-08-25 21:33:32 +0600

    init

---
+++ trunk/Z-Minifier/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Z_Minifier._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Z-Minifier</title>
+    <link href="Style.css" type="text/css" rel="Stylesheet" />
+</head>
+<body>
+<form id="form1" runat="server">
+<div align="center">
+    <div id="Container">
+    <h1>Z-Minifier</h1>
+    <p>This tool will allows you to have your javascript file minified. The minification code used for this is from <a href="http://www.crockford.com/javascript/jsmin.html">crockford.com</a>.</p>
+    <br />
+        Input File: <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <asp:Label ID="LabelMessage" runat="server"></asp:Label>
+        <br />
+        <hr noshade />
+        <asp:Button runat="server" ID="btnMinify" Text="Minify!" 
+            onclick="btnMinify_Click" />
+    </div>
+    &copy; 2009 Zach Curtis
+    </div>
+</form>
+</body>
+</html>
---
+++ trunk/Z-Minifier/Default.aspx.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Z_Minifier
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        const int EOF = -1;
+
+        StreamReader sr;
+        StreamWriter sw;
+        int theA;
+        int theB;
+        int theLookahead = EOF;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            { 
+            }
+        }
+
+
+        protected void btnMinify_Click(object sender, EventArgs e)
+        {
+          
+            //string[] args = e.ToString().Split(',');
+            //if (e.Length != 2)
+            //{
+            //    lblMessage.InnerText = "invalid arguments, 2 required, 1 in, 1 out";
+            //    return;
+            //}
+            //new JavaScriptMinifier().Minify(args[0], args[1]);
+
+            if (FileUpload1.PostedFile.FileName == "")
+            {
+                LabelMessage.Text = "No file specified.";
+            }
+            else
+            {
+                try
+                {
+                    string serverFileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    
+                    string srcFile = MapPath("Files/") + serverFileName;
+                    string destFile = MapPath("Files/") + serverFileName + ".min" + ".js";
+                    //FileInput.PostedFile.SaveAs(@"c:\" + serverFileName);
+                    FileUpload1.PostedFile.SaveAs(MapPath("Files/") + serverFileName);
+                    Minify(srcFile, destFile);
+                    LabelMessage.Text = "File <b>" + serverFileName;
+                    LabelMessage.Text += "</b> uploaded successfully!<br />";
+                    LabelMessage.Text += "<a href=\"Files/" + serverFileName + "\" title=\"Download Original\" target=\"_blank\">Download Original</a> (" + FileUpload1.PostedFile.ContentLength + ") bytes";
+                    FileInfo fi = new FileInfo(destFile);
+                    string outFileLength = fi.Length.ToString();
+                    LabelMessage.Text += "&nbsp; | &nbsp;<a href=\"Files/" + serverFileName + ".min" + ".js\" title=\"Download Minified\" target=\"_blank\">Download Minified</a> (" + outFileLength + ") bytes";
+                }
+                catch (Exception err)
+                {
+                    LabelMessage.Text = err.Message;
+                }
+            }
+            
+        }
+
+        public void Minify(string src, string dst)
+        {
+            using (sr = new StreamReader(src))
+            {
+                using (sw = new StreamWriter(dst))
+                {
+                    jsmin();
+                }
+            }
+        }
+
+        void jsmin()
+        {
+            theA = '\n';
+            action(3);
+            while (theA != EOF)
+            {
+                switch (theA)
+                {
+                    case ' ':
+                        {
+                            if (isAlphanum(theB))
+                            {
+                                action(1);
+                            }
+                            else
+                            {
+                                action(2);
+                            }
+                            break;
+                        }
+                    case '\n':
+                        {
+                            switch (theB)
+                            {
+                                case '{':
+                                case '[':
+                                case '(':
+                                case '+':
+                                case '-':
+                                    {
+                                        action(1);
+                                        break;
+                                    }
+                                case ' ':
+                                    {
+                                        action(3);
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        if (isAlphanum(theB))
+                                        {
+                                            action(1);
+                                        }
+                                        else
+                                        {
+                                            action(2);
+                                        }
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            switch (theB)
+                            {
+                                case ' ':
+                                    {
+                                        if (isAlphanum(theA))
+                                        {
+                                            action(1);
+                                            break;
+                                        }
+                                        action(3);
+                                        break;
+                                    }
+                                case '\n':
+                                    {
+                                        switch (theA)
+                                        {
+                                            case '}':
+                                            case ']':
+                                            case ')':
+                                            case '+':
+                                            case '-':
+                                            case '"':
+                                            case '\'':
+                                                {
+                                                    action(1);
+                                                    break;
+                                                }
+                                            default:
+                                                {
+                                                    if (isAlphanum(theA))
+                                                    {
+                                                        action(1);
+                                                    }
+                                                    else
+                                                    {
+                                                        action(3);
+                                                    }
+                                                    break;
+                                                }
+                                        }
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        action(1);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+
+        /* action -- do something! What you do is determined by the argument:
+               1   Output A. Copy B to A. Get the next B.
+               2   Copy B to A. Get the next B. (Delete A).
+               3   Get the next B. (Delete B).
+          action treats a string as a single character. Wow!
+          action recognizes a regular expression if it is preceded by ( or , or =.
+       */
+        void action(int d)
+        {
+            if (d <= 1)
+            {
+                put(theA);
+            }
+            if (d <= 2)
+            {
+                theA = theB;
+                if (theA == '\'' || theA == '"')
+                {
+                    for (; ; )
+                    {
+                        put(theA);
+                        theA = get();
+                        if (theA == theB)
+                        {
+                            break;
+                        }
+                        if (theA <= '\n')
+                        {
+                            throw new Exception(string.Format("Error: JSMIN unterminated string literal: {0}\n", theA));
+                        }
+                        if (theA == '\\')
+                        {
+                            put(theA);
+                            theA = get();
+                        }
+                    }
+                }
+            }
+            if (d <= 3)
+            {
+                theB = next();
+                if (theB == '/' && (theA == '(' || theA == ',' || theA == '=' ||
+                                    theA == '[' || theA == '!' || theA == ':' ||
+                                    theA == '&' || theA == '|' || theA == '?' ||
+                                    theA == '{' || theA == '}' || theA == ';' ||
+                                    theA == '\n'))
+                {
+                    put(theA);
+                    put(theB);
+                    for (; ; )
+                    {
+                        theA = get();
+                        if (theA == '/')
+                        {
+                            break;
+                        }
+                        else if (theA == '\\')
+                        {
+                            put(theA);
+                            theA = get();
+                        }
+                        else if (theA <= '\n')
+                        {
+                            throw new Exception(string.Format("Error: JSMIN unterminated Regular Expression literal : {0}.\n", theA));
+                        }
+                        put(theA);
+                    }
+                    theB = next();
+                }
+            }
+        }
+        /* next -- get the next character, excluding comments. peek() is used to see
+                if a '/' is followed by a '/' or '*'.
+        */
+        int next()
+        {
+            int c = get();
+            if (c == '/')
+            {
+                switch (peek())
+                {
+                    case '/':
+                        {
+                            for (; ; )
+                            {
+                                c = get();
+                                if (c <= '\n')
+                                {
+                                    return c;
+                                }
+                            }
+                        }
+                    case '*':
+                        {
+                            get();
+                            for (; ; )
+                            {
+                                switch (get())
+                                {
+                                    case '*':
+                                        {
+                                            if (peek() == '/')
+                                            {
+                                                get();
+                                                return ' ';
+                                            }
+                                            break;
+                                        }
+                                    case EOF:
+                                        {
+                                            throw new Exception("Error: JSMIN Unterminated comment.\n");
+                                        }
+                                }
+                            }
+                        }
+                    default:
+                        {
+                            return c;
+                        }
+                }
+            }
+            return c;
+        }
+        /* peek -- get the next character without getting it.
+        */
+        int peek()
+        {
+            theLookahead = get();
+            return theLookahead;
+        }
+        /* get -- return the next character from stdin. Watch out for lookahead. If
+                the character is a control character, translate it to a space or
+                linefeed.
+        */
+        int get()
+        {
+            int c = theLookahead;
+            theLookahead = EOF;
+            if (c == EOF)
+            {
+                c = sr.Read();
+            }
+            if (c >= ' ' || c == '\n' || c == EOF)
+            {
+                return c;
+            }
+            if (c == '\r')
+            {
+                return '\n';
+            }
+            return ' ';
+        }
+        void put(int c)
+        { 
+            sw.Write((char)c);
+        }
+        /* isAlphanum -- return true if the character is a letter, digit, underscore,
+                dollar sign, or non-ASCII character.
+        */
+        bool isAlphanum(int c)
+        {
+            return ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') ||
+                (c >= 'A' && c <= 'Z') || c == '_' || c == '$' || c == '\\' ||
+                c > 126);
+        }
+    }
+}
